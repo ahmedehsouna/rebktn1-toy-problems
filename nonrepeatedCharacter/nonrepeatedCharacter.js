4dfcc8e1656c244c205a0ef8bbdf5adf3aeba6b5
Moriarty <ahmedehsouna@yahoo.com>
2019-11-25 08:54:40 +0100

    done

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -7,5 +7,19 @@
  */
 
 var firstNonRepeatedCharacter = function(string) {
-  // TODO: your solution here
+	  // TODO: your solution here
+	var firstNonRepeated = ""
+	var array = string.split("")
+	for (var i = 0; i < array.length; i++) {
+	
+	var myCheckPoint = array.filter(one => one == array[i])
+	if(myCheckPoint.length == 1){
+		firstNonRepeated = myCheckPoint[0]
+		i = array.length
+	}
+}
+return firstNonRepeated
 };
+
+console.log(firstNonRepeatedCharacter('ABA'))
+console.log(firstNonRepeatedCharacter('AACBDB'))
