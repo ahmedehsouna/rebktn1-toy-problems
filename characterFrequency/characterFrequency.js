10a4f924916acecab81d5068d19a3169a209eea5
Moriarty <ahmedehsouna@yahoo.com>
2019-12-05 09:20:50 +0100

    sorted lol

--- characterFrequency/characterFrequency.js
+++ characterFrequency/characterFrequency.js
@@ -14,7 +14,18 @@ for (var i = 0; i < str.length; i++) {
 	if(obj[str[i]]) obj[str[i]]++
 	else obj[str[i]] = 1
 }
-return Object.entries(obj)
+return sorter(Object.entries(obj))
 }
 
+var sorter = function(arr){
+	for (var i = 0; i < arr.length; i++) {
+		for (var j = 0; j < arr.length - 1 - i; j++) {
+		if(arr[j][1] < arr[j+1][1])
+			[arr[j] , arr[j+1]] = [arr[j +1] , arr[j]]
+	}
+	}
+	return arr
+	
+}
+console.log(characterFrequency("aaabbc"))
 console.log(characterFrequency("mississippi"))
