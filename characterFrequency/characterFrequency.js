2f74641589fc699e7de823175e4a565385c1b6c2
Moriarty <ahmedehsouna@yahoo.com>
2019-12-05 09:06:18 +0100

    ok

---
+++ characterFrequency/characterFrequency.js
@@ -0,0 +1,21 @@
+/*
+Character Frequency
+Write a function call characterFrequency that takes as its input a string and returns an array
+of arrays as shown below sorted in descending order by frequency
+and then by ascending order by character.
+characterFrequency("aaabbc") -->    [ [ "a", 3 ], [ "b", 2 ], [ "c", 1 ] ]
+characterFrequency("mississippi")  -->  [ [ "i", 4 ], [ "s", 4 ], [ "p", 2 ], [ "m", 1 ] ]
+characterFrequency("") -->  [ ]
+*/
+
+var characterFrequency = function(str){
+var obj = {}
+for (var i = 0; i < str.length; i++) {
+	console.log(obj[str[i]])
+	if(obj[str[i]]) obj[str[i]]++
+	else obj[str[i]] = 1
+}
+return Object.entries(obj)
+}
+
+console.log(characterFrequency(""))
