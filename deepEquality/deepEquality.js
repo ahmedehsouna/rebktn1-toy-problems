b94f783857d15f0fa5a0909deaf17088fc8335b2
Moriarty <ahmedehsouna@yahoo.com>
2019-12-12 09:23:26 +0100

    without stringifyin:((

--- deepEquality/deepEquality.js
+++ deepEquality/deepEquality.js
@@ -12,8 +12,29 @@
   *
   */
 var deepEquals = function(apple, orange) {
-  return JSON.stringify(apple) == JSON.stringify(orange)
+  if(!Array.isArray(apple))
+  apple = Object.entries(apple)
+  if(!Array.isArray(orange))
+  orange = Object.entries(orange)
+  if(!(apple.length == orange.length)) return false 
+    for (var i = 0; i < apple.length; i++) {
+      if(Array.isArray(apple)){
+      if(typeof (apple[i]) == "object"){
+        if(!deepEquals(apple[i], orange[i])) return false
+      }else if(apple[i] != orange[i]) return false 
+      }else{
+      if(typeof (apple[i][1]) == "object"){
+        if(!deepEquals(apple[i][1], orange[i][1])) return false
+      }else if(apple[i][0] != orange[i][0] || apple[i][1] != orange[i][1]) return false        
+      }
+
+
+      
+    }
+  return true
+  // return JSON.stringify(apple) == JSON.stringify(orange)
 };
 
-  console.log( deepEquals({a:1, b: {c:3}},{a:1, b: {c:3}})); 
-  console.log( deepEquals({a:1, b: {c:5}},{a:1, b: {c:6}})); 
+  console.log( deepEquals({a:1, b: {c:3}},{a:1, b: {c:3}})); //true
+  console.log( deepEquals({a:1, b: {c:5}},{a:1, b: {c:6}})); //false
+  console.log( deepEquals({a:[1,2,{g:3}], b: {c:3}},{a:[1,2,{f:3}], b: {c:3}})); //deeep false lol
