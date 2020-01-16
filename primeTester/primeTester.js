8520cda92c3e15ddca0cc4c86391e4e9995ccd5d
Moriarty <ahmedehsouna@yahoo.com>
2020-01-16 09:04:09 +0100

    refactorin to increase performance

--- primeTester/primeTester.js
+++ primeTester/primeTester.js
@@ -10,6 +10,7 @@ primeTester(1) // false
 
 function primeTester(n) {
     if(n<2) return false
+    if((n-1)%6 != 0 && (n+1) % 6 != 0 && n>4) return false
     for (let i = 2; i < n; i++) {
         if(n%i == 0) return false
     }
